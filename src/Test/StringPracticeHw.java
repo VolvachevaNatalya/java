015f665a0cf0a4c33a3f62b2c509cc2c8b3a5fba
natalya-exelweb <natalya@exelweb.co.il>
2021-04-12 08:42:54 +0300

    2021_04_09_HomeWork (Tests)

---
+++ src/Test/StringPracticeHw.java
@@ -0,0 +1,35 @@
+package Test;
+
+public class StringPracticeHw {
+    public static String getStrReverse(String str) {
+        String rev = "";
+        if (str == null) {
+            return null;
+        } else if (str.trim().isEmpty()) {
+            return null;
+        }
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev += str.charAt(i);
+        }
+        return rev;
+    }
+
+    public static Integer getCountWords(String sent, String word) {
+        if (sent == null || word == null) {
+            return null;
+        } else if (sent.trim().isEmpty() || word.trim().isEmpty()) {
+            return null;
+        }
+        int count = 0;
+        String[] strs = sent.split(" ");
+        for (int i = 0; i <strs.length ; i++) {
+            if(strs[i].toLowerCase().equals(word.trim().toLowerCase())) {
+                count ++;
+            }
+        }
+        return count;
+
+    }
+
+}
+
---
+++ src/Test/StringPracticeHwTest.java
@@ -0,0 +1,25 @@
+package Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+class StringPracticeHwTest {
+
+    @Test
+    void getStrReverse() {
+        assertEquals("olleH", StringPracticeHw.getStrReverse("Hello"));
+        assertNull(StringPracticeHw.getStrReverse(""));
+        assertNull(StringPracticeHw.getStrReverse("  "));
+        assertNull(StringPracticeHw.getStrReverse(null));
+    }
+
+    @Test
+    void getCountWords() {
+        String sent = "Two beers or not two beers";
+        assertEquals(2, StringPracticeHw.getCountWords(sent,"two"));
+        assertEquals(0, StringPracticeHw.getCountWords(sent,"three"));
+        assertNull(StringPracticeHw.getCountWords(sent, null));
+        assertNull(StringPracticeHw.getCountWords(null, "two"));
+        assertNull(StringPracticeHw.getCountWords(null, null));
+        assertNull(StringPracticeHw.getCountWords(sent, "   "));
+        assertNull(StringPracticeHw.getCountWords("  ", ""));
+    }
+}
